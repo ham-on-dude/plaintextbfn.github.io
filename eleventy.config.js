73a8cc8ecf665b099c0ad4b637ec78b284024571
ham-on-dude <130493388+ham-on-dude@users.noreply.github.com>
2026-07-28 15:02:23 -0700

    Remove passthrough copy for style.css

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,7 +1,4 @@
 module.exports = function(eleventyConfig) {
-  // Copy your CSS or static assets straight through to the output folder if needed
-  eleventyConfig.passthroughCopy("style.css"); // (if you separate your CSS out)
-
   return {
     dir: {
       input: ".",
